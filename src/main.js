7f2d02f4cc736bcca3abc76a7d5dd97af6afe8ad
valerijmedvid <vmedvid@gmail.com>
2024-01-07 20:42:27 +0100

    add gtag

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
-// import VueGtag from "vue-gtag"
+import VueGtag from "vue-gtag"
 import VueSilentbox from "vue-silentbox"
 import "vue-silentbox/dist/style.css"
-
-// Vue.use(VueGtag, {
-//   config: { id: "G-HMQ5R408FB" },
-// })
 
 const myApp = createApp(App)
 
 myApp.use(router)
 myApp.use(VueSilentbox)
+myApp.use(
+  VueGtag,
+  {
+    config: { id: "G-HMQ5R408FB" },
+  },
+  router
+)
 
 myApp.mount("#app")
